7307700fc3ba98c1f4d6be031c55c25233eb302f
doublezerowld <jaba7012@proton.me>
2026-05-08 13:50:07 +0200

    Update barebones.rs

--- examples/barebones.rs
+++ examples/barebones.rs
@@ -7,7 +7,18 @@ impl CommandOutput for NoOutput {
     fn execute(&mut self, _: Command) {} // do nothing
 }
 
+struct NoRead;
+impl Read for NoRead {
+    fn read<R>(&self, _: R) -> Result<u8, ay_psg::errors::Error> {
+        Ok(0)
+    }
+}
+
 fn main() {
     let out = NoOutput {};
+
+    #[cfg(not(feature = "read"))]
     PSG::new(out, 2_000_000);
+    #[cfg(feature = "read")]
+    PSG::new(out, 2_000_000, ReadDriver(NoRead));
 }
